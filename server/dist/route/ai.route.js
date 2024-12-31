a8e7916489c8f44b026c36db28586b0f2e05a08b
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-12-31 10:59:44 +0530

    remove unused files and routes from the server distribution

--- server/dist/route/ai.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ai_controller_1 = require("../controller/ai.controller");
-const aiRoute = (0, express_1.Router)();
-aiRoute.post("/ask", ai_controller_1.getAians);
-//@ts-ignore
-aiRoute.get("/history", ai_controller_1.gethistory);
-exports.default = aiRoute;
